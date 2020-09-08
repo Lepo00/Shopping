52c71d601187860bdd83f269b3629d67900a5903
Lepo00 <luca.leporati@outlook.com>
2020-09-08 11:35:22 +0200

    feature module eliminato

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { FeaturesModule } from './features/features.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +13,6 @@ import { FeaturesModule } from './features/features.module';
     BrowserModule,
     AppRoutingModule,
     CoreModule,
-    FeaturesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
