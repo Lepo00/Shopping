af8db8bb4f09902a6b20e4aab65f35f3400e972a
Lepo00 <luca.leporati@outlook.com>
2020-09-08 17:47:20 +0200

    creazione menu component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {FormsModule} from '@angular/forms'
+import {FormsModule} from '@angular/forms';
+import { MenuComponent } from './menu/menu.component'
 
 
 @NgModule({
-  declarations: [],
+  declarations: [MenuComponent],
   imports: [
     CommonModule,
     FormsModule,
