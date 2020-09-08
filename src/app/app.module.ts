fd50c3d15adff722fd3d2280e527c0fa055d24be
Lepo00 <luca.leporati@outlook.com>
2020-09-08 11:03:06 +0200

    import 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { FeaturesModule } from './features/features.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { CoreModule } from './core/core.module';
     BrowserModule,
     AppRoutingModule,
     CoreModule,
+    FeaturesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
