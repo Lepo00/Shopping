c37a391f7dfd5a692594789cacb49738ccc0b1ca
Lepo00 <luca.leporati@outlook.com>
2020-09-08 11:19:10 +0200

    corretto

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home/home.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [],
   imports: [
     CommonModule
   ],
   exports: [
-    HomeComponent,
   ]
 
 })
