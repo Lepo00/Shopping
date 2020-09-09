942ccfc7bdb2eeae403da0c79b925ffd72d77989
Lepo00 <luca.leporati@outlook.com>
2020-09-09 16:44:17 +0200

    registrazione-login e img home

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -8,7 +8,7 @@ import { NgbCarousel, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/n
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  images = ["../../../assets/img/atalanta.jpeg","../../../assets/img/milan.jpeg","../../../assets/img/barcellona.jpg","../../../assets/img/inter.jpeg","../../../assets/img/liverpool.jpg"];
+  images = ["../../../assets/img/calcio.jpg","../../../assets/img/calcio-2.jpg","../../../assets/img/calcio-3.jpg","../../../assets/img/calcio-4.jpg","../../../assets/img/calcio-5.jpg"];
 
   paused = false;
   unpauseOnArrow = false;
