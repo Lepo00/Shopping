75595a1ffdc60a326a55d9e3019ef64e87d0ad50
Lepo00 <luca.leporati@outlook.com>
2020-09-09 18:27:45 +0200

    porcoddddddddddddd

--- src/app/redux/cart/cart.effects.ts
+++ src/app/redux/cart/cart.effects.ts
@@ -1,9 +1,9 @@
 import {Injectable} from "@angular/core";
 import { addToCart, saveToCart, initCart, retrieveAllProducts } from './cart.actions';
-import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { Actions, createEffect, ofType, act } from "@ngrx/effects";
 import { Observable } from 'rxjs';
 import { HttpCommunicationsService } from 'src/app/core/services/http-communications.service';
-import { switchMap, map } from 'rxjs/operators';
+import { switchMap, map, mergeMap } from 'rxjs/operators';
 import { Action } from '@ngrx/store';
 import { Product } from 'src/app/core/models/product';
 
@@ -11,15 +11,17 @@ import { Product } from 'src/app/core/models/product';
 export class CartEffects{
     addToCart$: Observable<Action> = createEffect(() => this.actions$.pipe(
         ofType(addToCart),
-        switchMap(action => this.httpCommunicationsService.retrievePostCall<{prod: Product, id: number}>("products", name).pipe(
-            map(product => saveToCart({ product: name }))
+        switchMap((action) => this.httpCommunicationsService.retrievePostCall("products",action.product)
+        .pipe(
+            map(() => saveToCart({product: action.product}))
         ))
     ));
 
     retrieveAllProducts$: Observable<Action> = createEffect(()=>this.actions$.pipe(
         ofType(retrieveAllProducts),
-        switchMap(action => this.httpCommunicationsService.retrieveGetCall<{name: Product, id: number}[]>("products").pipe(
-            map(products => initCart({ products: products.map(item => name) }))
+        switchMap(action => this.httpCommunicationsService.retrieveGetCall<Product[]>("products")
+        .pipe(
+            map(products => initCart({ products: products }))
         ))
     ))
 
